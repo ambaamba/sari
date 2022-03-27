53a572fc00bb7504a069d32479e39799af17fb5a
ambaamba <barbattini.angelo@gmail.com>
2022-03-27 08:36:15 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+const accuracy = 12
+const gravity = 400
+const clothHeight = 38
+const clothWidth = 70
+const spacing = 10
+const tearDist = 200
+const friction = 0.7
+const bounce = 0.6
+
+// Original settings
+// const accuracy = 20
+// const gravity = 400
+// const clothHeight = 38
+// const clothWidth = 70
+// const spacing = 14
+// const tearDist = 60
+// const friction = 0.99
+// const bounce = 0.5
